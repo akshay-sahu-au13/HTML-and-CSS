f4404889a076db6fb6c378ae5c208599e610ce53
Akshay Sahu <akki5233@gmail.com>
2021-01-16 00:34:36 +0530

    OS Module

---
+++ Nodejs/Node crash course/Reference/os_demo.js
@@ -0,0 +1,30 @@
+const os =  require('os');
+
+// Platform
+console.log(os.platform());
+
+// CPU archeitecture
+
+console.log(os.arch());
+
+//CPU Core info
+
+console.log(os.cpus())
+
+// Free memory
+
+console.log(os.freemem());
+
+//Total memory
+
+console.log(os.totalmem());
+
+// Home directory
+
+console.log(os.homedir());
+
+//Up time
+
+console.log(os.uptime());
+
+
