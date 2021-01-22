d75b2196aea27d635c71cd3ec5794e38622d6ef7
Akshay Sahu <akki5233@gmail.com>
2021-01-22 18:36:19 +0530

    Checkdigit function

---
+++ Questions/checkDigit.js
@@ -0,0 +1,18 @@
+function checkDigit(membershipId) {
+    // your code here
+    let digit = 0;
+    while (membershipId != 0){
+        digit += membershipId % 10;
+        membershipId = Math.floor(membershipId/10); 
+    };
+
+    if (digit >= 10) {
+        return checkDigit(digit)
+    }
+    else {
+        return digit
+    }
+
+    };
+
+console.log(checkDigit(55555))
