a28a594671486deb390303fb163832afe4f7dbca
Akshay Sahu <akki5233@gmail.com>
2021-02-11 02:00:33 +0530

    User auth, Mongodb, JWT, express

--- Mongodb/Blog app using Mongo node express auth/routes/users.js
+++ Mongodb/Blog app using Mongo node express auth/routes/users.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const jwt = require('jsonwebtoken');
-const {check, validationResult} = require('express-validator/check');
+const { check, validationResult } = require('express-validator/check');
 const router = express.Router();
 const bcrypt = require('bcrypt');
 const User = require('../schemas/User');
@@ -8,68 +8,68 @@ const auth = require('../middleware/auth');
 const User = require('../Schemas/userSchema');
 
 router.post('/signup',
-[
-    check('firstName', 'Please Enter the first name').not().isEmpty(),
-    check('email', 'Please enter the email').isEmail(),
-    check('password', 'Please enter the password').islength({min:6})
-],
-async (req, res)=> {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-        return res.status(400).json({
-            data: {},
-            errors: errors.array(),
-            message: "Unable to Sign-Up user"
-        })
-    }
-
-    try {
-        const user = await User.findOne({email: req.body.email});
-
-        if (user) {
+    [
+        check('firstName', 'Please Enter the first name').not().isEmpty(),
+        check('email', 'Please enter the email').isEmail(),
+        check('password', 'Please enter the password').islength({ min: 6 })
+    ],
+    async (req, res) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
             return res.status(400).json({
                 data: {},
-                error: [{
-                    value: req.body.email,
-                    msg: "user already exists!",
-                    param: 'email',
-                    location: 'body'
-                }],
-                message: 'Unable to create User, please login or enter a different email ID'
-            });
+                errors: errors.array(),
+                message: "Unable to Sign-Up user"
+            })
         }
 
-        user = new User({
-            firstName: req.body.firstName,
-            lastName: req.body.lastName || "",
-            email: req.body.email,
-        })
-        const salt = await bcrypt.genSalt(10);
-        user.password = await bcrypt(req.body.password, salt);
+        try {
+            const user = await User.findOne({ email: req.body.email });
 
-        await user.save();
+            if (user) {
+                return res.status(400).json({
+                    data: {},
+                    error: [{
+                        value: req.body.email,
+                        msg: "user already exists!",
+                        param: 'email',
+                        location: 'body'
+                    }],
+                    message: 'Unable to create User, please login or enter a different email ID'
+                });
+            }
 
-        res.status(200).json({
-            data: req.body,
-            errors: [],
-            message: "User successfully created!"
-        });
+            user = new User({
+                firstName: req.body.firstName,
+                lastName: req.body.lastName || "",
+                email: req.body.email,
+            })
+            const salt = await bcrypt.genSalt(10);
+            user.password = await bcrypt(req.body.password, salt);
 
-    } catch(e) {
-        console.log(e);
-        res.status(500).send("Error in saving...")
-    }
-});
+            await user.save();
 
-router.post('/login', 
+            res.status(200).json({
+                data: req.body,
+                errors: [],
+                message: "User successfully created!"
+            });
+
+        } catch (e) {
+            console.log(e);
+            res.status(500).send("Error in saving...")
+        }
+    });
+
+router.post('/login',
     [
         check('email', 'Please enter a valid email').isEmail(),
         check('password', 'Please enter the correct Passowrd').not().isEmpty()
     ],
-    async(req, res) => {
+    async (req, res) => {
         const errors = validationResult(req);
 
-        if(!errors.isEmpty()) {
+        if (!errors.isEmpty()) {
             return res.status(400).json({
                 data: {},
                 errors: errors.array(),
@@ -77,35 +77,55 @@ router.post('/login',
             });
         }
 
-        const user = await User.findOne({email: req.body.email});
-        if (!user) {
-            return res.status(404).json({
-                data: {},
-                errors: [{
-                    value: req.body.email,
-                    msg: "Email is not registered",
-                    param: 'email',
-                    location: 'body'
-                }],
-                message: "Unable to Login!"
-            });
-        };
+        try {
+            let user = await User.findOne({ email: req.body.email });
+            if (!user) {
+                return res.status(404).json({
+                    data: {},
+                    errors: [{
+                        value: req.body.email,
+                        msg: "Email is not registered",
+                        param: 'email',
+                        location: 'body'
+                    }],
+                    message: "Unable to Login!"
+                });
+            };
 
-        const isMatch = await bcrypt.compare(req.body.password, user.password);
-        if (!isMatch) {
-            res.status(400).json({
-                data: {},
-                errors: [{
-                    value: req.body.password,
-                    msg: "Incorrect Password",
-                    param: "password",
-                    location: "body"
-                }],
-                message: 'Unable to login, please enter the correct password!'
-            });
+            const isMatch = await bcrypt.compare(req.body.password, user.password);
+            if (!isMatch) {
+                res.status(400).json({
+                    data: {},
+                    errors: [{
+                        value: req.body.password,
+                        msg: "Incorrect Password",
+                        param: "password",
+                        location: "body"
+                    }],
+                    message: 'Unable to login, please enter the correct password!'
+                });
+            }
+
+            jwt.sign(
+                { user: { id: user.id } },
+                'jwt_secret',
+                (err, token) => {
+                    if (err) {
+                        throw err
+                    }
+                    res.status(200).json({
+                        data: { token },
+                        errors: [],
+                        message: "Login Success!"
+                    });
+                }
+            )
+        } catch (e) {
+            console.log(e.message);
+            res.status(500).send("Error in login");
+
         }
 
-        
     }
-);  
+);
 
