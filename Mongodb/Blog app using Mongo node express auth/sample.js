899f3e5c44bc73f40a1e6be9f3dd166536900f77
Akshay Sahu <akki5233@gmail.com>
2021-02-11 22:10:05 +0530

    Updates

---
+++ Mongodb/Blog app using Mongo node express auth/sample.js
@@ -0,0 +1,84 @@
+{
+    "USER": {
+        "createdAt": "2021-02-11T07:38:12.250Z",
+        "_id": "6024dff36f143e0fa4eba7fb",
+        "firstName": "Akshay",
+        "lastName": "sahu",
+        "email": "akshay@gmail.com",
+        "todos": [
+            {
+                "status": false,
+                "_id": "6024e0916f143e0fa4eba7fc",
+                "title": "Blog Post 1"
+            }
+        ],
+        "password": "$2a$10$RRU6jPD21BWgpv/YjEOYyOH6lKNexHv0Rw7L8klvUFTNytEJW.s3e",
+        "__v": 1
+    },
+    "BLOGS": [
+        {
+            "userId": "6024dff36f143e0fa4eba7fb",
+            "createdAt": "2021-02-11T07:38:12.300Z",
+            "_id": "6024e1366f143e0fa4eba7fd",
+            "title": "Blog Post 1",
+            "body": "This is just a random blog post for testing purpose.",
+            "__v": 0
+        },
+        {
+            "userId": "6024dff36f143e0fa4eba7fb",
+            "createdAt": "2021-02-11T13:46:22.383Z",
+            "_id": "60253531e893a9351cfc28c1",
+            "title": "Blog Post 2",
+            "body": "Hello there, What do you know about cosmology? Let us deep dive into some really cool facts about our Cosmos",
+            "__v": 0
+        },
+        {
+            "userId": "6024dff36f143e0fa4eba7fb",
+            "createdAt": "2021-02-11T13:46:22.383Z",
+            "_id": "60253570e893a9351cfc28c2",
+            "title": "Choose the perfect design",
+            "body": "Create a beautiful blog that fits your style. Choose from a selection of easy-to-use templates – all with flexible layouts and hundreds of background images – or design something new.",
+            "__v": 0
+        },
+        {
+            "userId": "6024dff36f143e0fa4eba7fb",
+            "createdAt": "2021-02-11T15:58:54.231Z",
+            "_id": "60255531c3f7002e9865cc3c",
+            "title": "Laptops under 50,000",
+            "body": "Kindly find below the lists of Laptops that you can purchase under 50K",
+            "__v": 0
+        },
+        {
+            "userId": "6024dff36f143e0fa4eba7fb",
+            "createdAt": "2021-02-11T15:58:54.231Z",
+            "_id": "6025559fc3f7002e9865cc3d",
+            "title": "Best Holiday destinations in India",
+            "body": "In this blog, you will find the most exotic and beautiful places to visit during your vaction in India",
+            "__v": 0
+        },
+        {
+            "userId": "6024dff36f143e0fa4eba7fb",
+            "createdAt": "2021-02-11T15:58:54.231Z",
+            "_id": "602555e9c3f7002e9865cc3e",
+            "title": "Who is the richest person on earth",
+            "body": "As per the current census, Jeff Bezos, the founder and owner of Amazon is the world's richest man",
+            "__v": 0
+        },
+        {
+            "userId": "6024dff36f143e0fa4eba7fb",
+            "createdAt": "2021-02-11T15:58:54.231Z",
+            "_id": "6025560ec3f7002e9865cc3f",
+            "title": "What can you do during your weekends",
+            "body": "FInd the list of all the useful and productive tasks that you can do during your weekend",
+            "__v": 0
+        },
+        {
+            "userId": "6024dff36f143e0fa4eba7fb",
+            "createdAt": "2021-02-11T15:58:54.231Z",
+            "_id": "60255662c3f7002e9865cc40",
+            "title": "Continental delicacies",
+            "body": "Let's explore the variety of tasty and mouth watering continental cuisines that you would love to eat daily",
+            "__v": 0
+        }
+    ]
+}
