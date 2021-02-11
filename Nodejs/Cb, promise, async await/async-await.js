bc8d91e945b75c10220fc7b3298eb8ff19c6582c
Akshay Sahu <akki5233@gmail.com>
2021-02-12 00:47:23 +0530

    Promises and async await

--- Nodejs/Cb, promise, async await/async-await.js
+++ Nodejs/Cb, promise, async await/async-await.js
@@ -1,5 +1,3 @@
-
-
 // Let's refactor our code from callback hell using Async Await
 
 console.log('Starting');
@@ -34,26 +32,6 @@ function videoNames(video) {
     });
 }
 
-// login('akshay@gmail.com', 'Akshay', info => {
-//     console.log(info)
-//     userVideos(['Cartoon video', 'Bollywood movie', 'Animation video'], videos => {
-//         console.log(videos)
-//         videoNames(['Jungle Book', '3 idiots', 'Avengers'], vidname => {
-//             console.log(vidname);
-
-//         });
-//     });
-// });
-
-// const users = login("xyz@gmail.com", 'Ravi');
-
-// users.then(data => userVideos(data.email))
-// .then(videos => videoNames(videos))
-// .then(names => {
-//     console.log(`The name of the video ${names.video[0]} is ${names.Cartoons}`)
-// })
-// .catch(err => console.log(err,"Couldn't fetch the data"))
-// console.log("Finished");
 
 
 async function msgapp () {
