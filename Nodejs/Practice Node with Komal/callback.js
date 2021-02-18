e73ec918bcf5e2befb13c79d0f63354e1f9c55f8
Akshay Sahu <akki5233@gmail.com>
2021-02-18 22:42:45 +0530

    Promise practice

--- Nodejs/Practice Node with Komal/callback.js
+++ Nodejs/Practice Node with Komal/callback.js
@@ -2,34 +2,75 @@
 
 console.log("Starting");
 
-function login(email, pwd, cb) {
-    setTimeout(() => {
-        // console.log("The function ended now!")
-        cb({email, pwd})
-    }, 4000);
+function login(email, pwd) {
+
+    return new Promise ((res, rej)=> {
+
+        setTimeout(() => {
+            res({email, pwd});
+            rej("error")
+        }, 4000);
+    })
 };
 
-function usermsg(useremail, cb) {
-    setTimeout(() => {
-        cb(['msg1', 'msg2', 'msg3']);
-    }, 3500);
+function usermsg(useremail) {
+    return new Promise((res, rej)=> {
+
+        setTimeout(() => {
+            res(['msg1', 'msg2', 'msg3']);
+            rej("error in fetching");
+        }, 3500);
+    })
 }
 
-function message (msgid , cb) {
-    setTimeout(() => {
-        cb({msgid});
-    }, 2000);
+function message (msgid ) {
+    return new Promise((res, rej)=>{
+
+        setTimeout(() => {
+            res({msgid});
+            rej("Error");
+        }, 2000);
+    })
 }
 //Callback hell
-login("kmishra@gmail", 'ertyhuj4567u8', data => {
+// login("kmishra@gmail", 'ertyhuj4567u8', data => {
+//     console.log(data);
+//     usermsg(data.email, msgs => {
+//         console.log(msgs);
+//         message(msgs[0], message => {
+//             console.log(message);
+//         });
+//     });
+// });
+
+
+// console.log("Finished");
+
+
+// login('akx@gmail.com', 123456).then(data => {
+//     console.log(data);
+//     usermsg(data.email).then(msg => {
+//         console.log(msg)
+//         message(msg[0]).then(message=> {
+//             console.log(message);
+//         })
+//     })
+// })
+
+login('akx@gmail.com', 123456).then(data => {
     console.log(data);
-    usermsg(data.email, msgs => {
-        console.log(msgs);
-        message(msgs[0], message => {
-            console.log(message);
-        });
-    });
+    return usermsg(data.email)
+ 
+}).then (msg => {
+    console.log(msg);
+    return message(msg[0]);
+
+}).then(message => {
+    console.log(message);
+
+}).catch(err => {
+    console.log(err);
 });
 
 
-console.log("Finished");
+
