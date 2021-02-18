5d158c468f7ec094c0781b2349419f5a8e5cc219
Akshay Sahu <akki5233@gmail.com>
2021-02-18 17:16:37 +0530

    Connecting PostgreSQL with node

---
+++ PGSQL/Integrating Postgres with Node/app.js
@@ -0,0 +1,37 @@
+const require = require('express');
+const app = express();
+
+const PORT = 9898;
+
+const Pool = require('pg').Pool;
+const pool = new Pool({
+    user:'postgres',
+    host:'localhost',
+    database:'postgres',
+    port:5432
+});
+
+app.get('/users', (req, res)=> {
+    pool.query('SELECT * FROM customers ORDER BY first_name ASC', (err, result)=> {
+        if (err) throw err;
+        res.json(result)
+    })
+})
+
+
+app.get('/users/:id', (req, res)=> {
+    pool.query(`SELECT * FROM customers WHERE id = ${id}`, (err, result)=> {
+        if (err) throw err;
+        res.json(result.rows)
+    })
+});
+
+app.post('/addUser', (req, res)=>{
+    const {first_name, last_name, gender, phone_number} = req.body;
+    pool.query('INSERT INTO customers VALUES ($1, $2, $3, $4)', [first_name, last_name, gender, phone_number], (err, result)=> {
+        if err throw err
+        res.send(result)
+    })
+});
+
+app,listen(PORT);
