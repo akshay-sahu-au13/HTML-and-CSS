a4824d693d415222808d7a9872ab04c4feaf795c
Akshay Sahu <akki5233@gmail.com>
2021-04-06 19:38:59 +0530

    app.js and home.html created -Checking post req

---
+++ CS and Assns/app.js
@@ -0,0 +1,43 @@
+
+const login = document.querySelector('#login');
+const email = document.querySelector("#email");
+const password = document.querySelector("#password");
+console.log(email.value)
+// console.log(`email: ${email}, password: ${password}`);
+
+
+
+login.addEventListener('click', function(e){
+    e.preventDefault();
+    const emailid = email.value;
+    const pwd = password.value;
+    postreq(emailid, pwd)
+    
+});
+async function postreq(email,pwd){
+    try {
+        const options = {
+            method: "POST",
+            body: JSON.stringify({
+                email: email,
+                password: pwd
+        }),
+            headers: {
+                "Content-Type": "application/json"
+            }
+        }
+
+        console.log("Options:",options)
+        let response = await fetch("https://reqres.in/api/login", options);
+        console.log("Response: ", response);
+        if (response.status == 200) {
+            window.location.replace('home.html')
+            console.log("Post request Successful");
+        } else {
+            console.log("POST request failed");
+        }
+    } catch (error) {
+        console.log("Error in post request");
+    }
+
+}
