4282d3271315912a409e22d2cce7e917cd642514
Akshay Sahu <akki5233@gmail.com>
2021-04-07 21:12:48 +0530

    Basic layout done and fetched data from API

--- CS and Assns/week33/Day03/app.js
+++ CS and Assns/week33/Day03/app.js
@@ -1,8 +1,8 @@
 
-$(".card").click(function(e){
-    console.log("Event: ",e);
-    displayData()
-});
+// $(".card").click(function(e){
+//     console.log("Event: ",e);
+//     displayData()
+// });
 
 
 $(document).ready(function (){
@@ -38,7 +38,29 @@ function displayData(data, status){
 
 $("#country").click(function (e) {
     console.log(e.target.value)
-    if (e.target.value){
+    if (e.target.value !== "none"){
+        
+        $(".card #global").css('display', 'none');
+        $(".card #start_data").css('display', 'none');
+        $(".card #covid_data").css('display', 'block')
 
+    } else {
+        
     }
 });
+
+{/* <div class="card">
+    <div id="global">
+
+    </div>
+
+    <div id="start_data">
+        <h2>Select a country to see the data</h2>
+    </div>
+    <div id="covid_data">
+        <h1 id="Country"></h1>
+        <div class="stats">
+            <span id="label"></span>
+            <span id="value"></span>
+        </div>
+    </div> */}
