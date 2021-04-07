0b9f9f7cfa2ae52c74ef57138d6fd3bc6eace0a9
Akshay Sahu <akki5233@gmail.com>
2021-04-07 21:49:18 +0530

    All functionalities done, Need some CSS editing

--- CS and Assns/week33/Day03/app.js
+++ CS and Assns/week33/Day03/app.js
@@ -5,12 +5,60 @@
 // });
 
 
-$(document).ready(function (){
+$(document).ready(async function (){
 
-    $.ajax({
+    let all_data = $.ajax({
         type: "GET",
         url: "https://api.covid19api.com/summary",
         success: displayData
+    });
+
+    const res = await all_data;
+    const global = res.global;
+    const country_data = res.Countries
+    console.log(country_data)
+
+    $("#country").click(function (e) {
+        console.log(e.target.value)
+        let country_name = e.target.value;
+        if (e.target.value !== "none") {
+            country_data.forEach(function(item){
+                if (item.Country === country_name) {
+
+
+                    
+                    $(".card #global").css('display', 'none');
+                    $(".card #start_data").css('display', 'none');
+                    $(".card #covid_data").css('display', 'block')
+                    $("#Country").text(`${country_name}`)
+                $(".stats").html(`
+                <div>
+                <span id="label">Total Confirmed: </span>
+                    <span id="value">${item.TotalConfirmed}</span>
+                </div>
+                <div>
+                    <span id="label">Total Deceased: </span>
+                    <span id="value">${item.TotalDeaths}</span>
+                    </div>
+                <div>
+                    <span id="label">Total Recovered: </span>
+                    <span id="value">${item.TotalRecovered}</span>
+                    </div>
+                    <div> 
+                    <span id="label">New Confirmed: </span>
+                    <span id="value">${item.NewConfirmed}</span>
+                </div>
+                <div>
+                <span id="label">New Recovered: </span>
+                <span id="value">${item.NewRecovered}</span>
+                </div>
+                <div id="last_updated">Last updated at: <span>${item.Date}</span></div>
+                
+                `)
+            }
+        });
+        
+    }
     });
 
 });
@@ -33,21 +81,47 @@ function displayData(data, status){
     all_countries.forEach(function(country){
         Country.append(`<option value=${country} class="name">${country}</option>`)
     });
+
+    return data
      
 }
 
-$("#country").click(function (e) {
-    console.log(e.target.value)
-    if (e.target.value !== "none"){
-        
-        $(".card #global").css('display', 'none');
-        $(".card #start_data").css('display', 'none');
-        $(".card #covid_data").css('display', 'block')
 
-    } else {
+
+// $("#country").click(function (e) {
+//     console.log(e.target.value)
+//     if (e.target.value !== "none"){
         
-    }
-});
+//         $(".card #global").css('display', 'none');
+//         $(".card #start_data").css('display', 'none');
+//         $(".card #covid_data").css('display', 'block')
+//         // $("#Country").text(`${}`)
+//         $(".stats").append(`
+//             <div>
+//                 <span id="label">Total Confirmed: </span>
+//                 <span id="value"></span>
+//             </div>
+//             <div>
+//                 <span id="label">Total Deceased: </span>
+//                 <span id="value"></span>
+//             </div>
+//             <div>
+//                 <span id="label">Total Recovered: </span>
+//                 <span id="value"></span>
+//             </div>
+//             <div>
+//                 <span id="label">New Confirmed: </span>
+//                 <span id="value"></span>
+//             </div>
+//             <div>
+//                 <span id="label">New Recovered: </span>
+//                 <span id="value"></span>
+//             </div>
+//             <div id="last_updated">Last updated at: <span></span></div>
+            
+//         `)
+//     } 
+// });
 
 {/* <div class="card">
     <div id="global">
