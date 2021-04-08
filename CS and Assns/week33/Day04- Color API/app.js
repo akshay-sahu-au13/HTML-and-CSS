501c6901f9e014215024228178b0ed465d7c1e4b
Akshay Sahu <akki5233@gmail.com>
2021-04-08 21:40:09 +0530

    Completed using for each

--- CS and Assns/week33/Day04- Color API/app.js
+++ CS and Assns/week33/Day04- Color API/app.js
@@ -1,2 +1,27 @@
 //   https://reqres.in/api/unknown
 
+function showColors(){
+
+}
+
+$(document).ready(async function(e){
+
+    const res = $.ajax({
+        type:"GET",
+        url: "https://reqres.in/api/unknown",
+        success: showColors
+    });
+
+    const colors = await res;
+    console.log(colors);
+
+    colors.data.forEach(function(item){
+        console.log(item)
+        if (item.year > 2001){
+            $('.container').append(`
+            <div class="color" style="background-color: ${item.color};"></div>
+            `)
+        }
+    });
+
+});
